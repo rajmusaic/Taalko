8be0468771c2b4f21f4d95dd96701720ae116592
rajmusaic <ytrudra95@gmail.com>
2026-02-27 16:17:15 +0530

    Replace old songs with new entries and covers
    Updated song list with new entries and cover images.

--- data.js
+++ data.js
@@ -4,24 +4,61 @@ const songs = [
     name: "Bul Bul",
     artist: "Muhfaad",
     url: "1.mp3", // Yahan song ka link daalo
-    cover: "1.jpg" // Yahan cover image ka path daalo
+    cover: "https://files.catbox.moe/kwmp7q.png" // Yahan cover image ka path daalo
   },
   {
-    name: "TFBOL",
-    artist: "Emiway Bantai",
+    name: "EK THA RAJA",
+    artist: "Noddy Khan",
     url: "2.mp3",
-    cover: "2.jpg"
+    cover: "https://files.catbox.moe/c1gk7u.png"
   },
   {
-    name: "DUB 45",
-    artist: "Sidhu moose wala",
+    name: "F_CK WHAT THEY SAY",
+    artist: "MC Stan x King",
     url: "3.mp3",
-    cover: "3.jpg"
+    cover: "https://files.catbox.moe/e2ggur.png"
   },
   {
-    name: "Lazanus",
-    artist: "emiway bantai",
+    name: "Freeverse Feast",
+    artist: "Kr$na",
     url: "4.mp3",
-    cover: "4.jpg"
-  }
+    cover: "https://files.catbox.moe/447ej0.png"
+  },
+    {
+    name: "Khuja mat",
+    artist: "MC Stan",
+    url: "3.mp3",
+    cover: "https://files.catbox.moe/e68mcm.png"
+  },
+    {
+    name: "LAZANUS",
+    artist: "Emiway bantai",
+    url: "2.mp3",
+    cover: "https://files.catbox.moe/40wf15.png"
+  },
+  {
+    name: "Machayenge 4",
+    artist: "Emiway bantai",
+    url: "3.mp3",
+    cover: "https://files.catbox.moe/ttf1io.png"
+  },
+  {
+    name: "NustaPaisa.",
+    artist: "MC Stan",
+    url: "4.mp3",
+    cover: "https://files.catbox.moe/xpmnui.png"
+  },
+    {
+    name: "ROKO 2MG",
+    artist: "Emiway bantai",
+    url: "3.mp3",
+    cover: "https://files.catbox.moe/i6pnas.png"
+  },
+     {
+    name: "Chusamba",
+    artist: "Emiway bantai",
+    url: "3.mp3",
+    cover: "https://files.catbox.moe/hymu7e.png"
+  },
 ];
+
