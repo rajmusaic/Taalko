ba06ef88cb324f69a55850a82cc2612a42fb2369
rajmusaic <ytrudra95@gmail.com>
2026-02-27 15:43:34 +0530

    Replace songs list with new entries and structure
    Updated the songs array with new song entries and modified the structure to use 'name' instead of 'title'.

---
+++ data.js
@@ -0,0 +1,27 @@
+/ Babu bro, yahan apne songs add kar lo!
+const songs = [
+  {
+    name: "Bul Bul",
+    artist: "Muhfaad",
+    url: "1.mp3", // Yahan song ka link daalo
+    cover: "1.jpg" // Yahan cover image ka path daalo
+  },
+  {
+    name: "TFBOL",
+    artist: "Emiway Bantai",
+    url: "2.mp3",
+    cover: "2.jpg"
+  },
+  {
+    name: "DUB 45",
+    artist: "Sidhu moose wala",
+    url: "3.mp3",
+    cover: "3.jpg"
+  },
+  {
+    name: "Lazanus",
+    artist: "emiway bantai",
+    url: "4.mp3",
+    cover: "4.jpg"
+  }
+];
