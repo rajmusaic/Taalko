8a54e0bf225ae41484645060e5ebeafda15a9cec
rajmusaic <ytrudra95@gmail.com>
2026-02-27 16:52:57 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,64 +1,63 @@
-/ Babu bro, yahan apne songs add kar lo!
+// Babu bro, yahan aapka updated songs data hai!
 const songs = [
   {
     name: "Bul Bul",
     artist: "Muhfaad",
-    url: "1.mp3", // Yahan song ka link daalo
-    cover: "https://files.catbox.moe/kwmp7q.png" // Yahan cover image ka path daalo
+    url: "https://raw.githubusercontent.com/rajmusaic/song_2x/main/bulbul_2x.mp3",
+    cover: "https://files.catbox.moe/kwmp7q.png"
   },
   {
     name: "EK THA RAJA",
     artist: "Noddy Khan",
-    url: "2.mp3",
+    url: "https://raw.githubusercontent.com/rajmusaic/song_2x/main/EK%20THA%20RAJA_2x.mp3",
     cover: "https://files.catbox.moe/c1gk7u.png"
   },
   {
     name: "F_CK WHAT THEY SAY",
     artist: "MC Stan x King",
-    url: "3.mp3",
+    url: "https://raw.githubusercontent.com/rajmusaic/song_2x/main/F_CK%20WHAT%20THEY%20SAY%20x2.mp3",
     cover: "https://files.catbox.moe/e2ggur.png"
   },
   {
     name: "Freeverse Feast",
     artist: "Kr$na",
-    url: "4.mp3",
+    url: "https://raw.githubusercontent.com/rajmusaic/song_2x/main/Freeverse%20Feast_2x.mp3",
     cover: "https://files.catbox.moe/447ej0.png"
   },
-    {
-    name: "Khuja mat",
+  {
+    name: "Khuja Mat",
     artist: "MC Stan",
-    url: "3.mp3",
+    url: "https://raw.githubusercontent.com/rajmusaic/song_x2/main/Khuja%20Mat_x2.mp3",
     cover: "https://files.catbox.moe/e68mcm.png"
   },
-    {
+  {
     name: "LAZANUS",
-    artist: "Emiway bantai",
-    url: "2.mp3",
+    artist: "Emiway Bantai",
+    url: "https://raw.githubusercontent.com/rajmusaic/song_x2/main/LAZANUS_x2.mp3",
     cover: "https://files.catbox.moe/40wf15.png"
   },
   {
     name: "Machayenge 4",
-    artist: "Emiway bantai",
-    url: "3.mp3",
+    artist: "Emiway Bantai",
+    url: "https://raw.githubusercontent.com/rajmusaic/song_x2/main/Machayenge%204_2x.mp3",
     cover: "https://files.catbox.moe/ttf1io.png"
   },
   {
-    name: "NustaPaisa.",
+    name: "Nusta Paisa",
     artist: "MC Stan",
-    url: "4.mp3",
+    url: "https://raw.githubusercontent.com/rajmusaic/song_x2/main/nustaPaisa_2x.mp3",
     cover: "https://files.catbox.moe/xpmnui.png"
   },
-    {
+  {
     name: "ROKO 2MG",
-    artist: "Emiway bantai",
-    url: "3.mp3",
+    artist: "Emiway Bantai",
+    url: "https://raw.githubusercontent.com/rajmusaic/song_x2/main/ROKO-2MG_x2.mp3",
     cover: "https://files.catbox.moe/i6pnas.png"
   },
-     {
+  {
     name: "Chusamba",
-    artist: "Emiway bantai",
-    url: "3.mp3",
+    artist: "Emiway Bantai",
+    url: "https://raw.githubusercontent.com/rajmusaic/song_2x/main/Chusamba_x2.mp3",
     cover: "https://files.catbox.moe/hymu7e.png"
-  },
+  }
 ];
-
